159ce38e0bb303dfcc892bf3897e775fe28c55be
Damian Carrillo <damian.carrillo@cdev.co>
2007-06-22 15:19:45 +0000

    Adding licensing information and class-level javadoc.

--- game-of-life/src/main/java/org/dcarrillo/life/Setup.java
+++ game-of-life/src/main/java/org/dcarrillo/life/Setup.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2007, Damian Carrillo
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright notice, 
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright 
+ *       notice, this list of conditions and the following disclaimer in the 
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <ORGANIZATION> nor the names of its 
+ *       contributors may be used to endorse or promote products derived from 
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER 
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.dcarrillo.life;
 
 import agave.HandlerContext;
@@ -14,6 +43,11 @@ import java.util.List;
 
 import org.antlr.stringtemplate.StringTemplate;
 
+/**
+ * Present the grid so that the user can set it up by bring cells to life.
+ * 
+ * @author <a href="mailto:damiancarrillo@gmail.com">Damian Carrillo>
+ */
 @Path("/setup")
 @PositionalParameters("pattern")
 @ContentType(ContentType.APPLICATION_XHTML_XML)
@@ -223,7 +257,7 @@ public final class Setup extends AbstractGridHandler {
 				grid.getCurrent().get(16).get(29).setAlive(true);
 				grid.getCurrent().get(17).get(16).setAlive(true);
 				grid.getCurrent().get(17).get(20).setAlive(true);
-				grid.getCurrent().get(18)ß.get(17).setAlive(true);
+				grid.getCurrent().get(18).get(17).setAlive(true);
 				grid.getCurrent().get(18).get(18).setAlive(true);
 				break;
 			default:
