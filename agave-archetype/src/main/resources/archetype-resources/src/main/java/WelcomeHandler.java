8da84b4a7527fbd801e33c681f756a6c9111ee56
damiancarrillo <damiancarrillo@16d111f7-cb32-0410-b349-65a5e00931ee>
2008-10-08 05:35:19 +0000

    Fixing strange jetty plugin error that was occurring on the samples because they used the most recent version

--- agave-archetype/src/main/resources/archetype-resources/src/main/java/WelcomeHandler.java
+++ agave-archetype/src/main/resources/archetype-resources/src/main/java/WelcomeHandler.java
@@ -1,35 +1,17 @@
 package $package;
 
 import agave.AbstractHandler;
+import agave.Destination;
 import agave.HandlesRequestsTo;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.PrintWriter;
 
 public class WelcomeHandler extends AbstractHandler {
 
     @HandlesRequestsTo("/")
-    public void welcome() throws Exception {
-        response.setContentType("application/xhtml+xml");
-        
-        String context = request.getContextPath().substring(1);
-        
-        PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"");
-        out.println("  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
-        out.println("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">");
-        out.println("  <head>");
-        out.println("    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
-        out.format("    <title>Hello, %s!</title>\n", context);
-        out.println("  </head>");
-        out.println("  <body>");
-        out.format("    <p>Hello, %s!</p>\n", context);
-        out.println("  </body>");
-        out.println("</html>");
-        
-        out.close();
+    public Destination welcome() throws Exception {
+        request.setAttribute("context", request.getContextPath().substring(1));
+        return new Destination("/WEB-INF/jsp/index.jsp");
     }
     
 }
