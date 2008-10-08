155f851c721effcf9da9c2304086ba5caa061d06
damiancarrillo <damiancarrillo@16d111f7-cb32-0410-b349-65a5e00931ee>
2008-10-08 21:46:01 +0000

    Adding content type to the default index.jsp

--- agave/agave-archetype/src/main/resources/archetype-resources/src/main/java/WelcomeHandler.java
+++ agave/agave-archetype/src/main/resources/archetype-resources/src/main/java/WelcomeHandler.java
@@ -10,6 +10,7 @@ public class WelcomeHandler extends AbstractHandler {
 
     @HandlesRequestsTo("/")
     public Destination welcome() throws Exception {
+        request.setAttribute("contextPath", request.getContextPath());
         request.setAttribute("context", request.getContextPath().substring(1));
         return new Destination("/WEB-INF/jsp/index.jsp");
     }
