bec64830941f2dbd6a0fc94e94efeba9512e1e68
Damian Carrillo <damian.carrillo@cdev.co>
2008-10-08 05:58:36 +0000

    Adding guestbook sample

---
+++ agave/agave-samples/guestbook/src/test/java/agave/samples/WelcomeHandlerTest.java
@@ -0,0 +1,13 @@
+package agave.samples;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WelcomeHandlerTest {
+
+    @Test
+    public void welcomeTest() throws Exception {
+        Assert.assertTrue(true);
+    }
+    
+}
