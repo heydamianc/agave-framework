343bffc587386392493c6ec968edd6f8dc7e8afd
Damian Carrillo <damian.carrillo@cdev.co>
2009-03-08 15:38:27 +0000

    Updating archetype to work with Agave 1.0 release

--- agave-archetype/src/main/resources/archetype-resources/src/main/java/WelcomeHandler.java
+++ agave-archetype/src/main/resources/archetype-resources/src/main/java/WelcomeHandler.java
@@ -1,16 +1,31 @@
-package $package;
+package org.dcarrillo;
 
-import agave.AbstractHandler;
 import agave.Destination;
+import agave.Destinations;
+import agave.HandlerContext;
 import agave.HandlesRequestsTo;
 
-public class WelcomeHandler extends AbstractHandler {
+import javax.servlet.http.HttpServletRequest;
 
+public class WelcomeHandler {
+
+    /**
+     * Serves the same purpose as that of a 'welcome-file' entry in the web.xml file.
+     * Note, that binding a handler to the root of the application like we are doing here will 
+     * always override the 'welcome-file' entry.
+     *
+     * @param handlerContext the context that this handler method executes under
+     * @throws Exception if anything goes wrong
+     * @return a destination object that wraps the index.jsp page
+     */
     @HandlesRequestsTo("/")
-    public Destination welcome() throws Exception {
+    public Destination welcome(HandlerContext handlerContext) throws Exception {
+        HttpServletRequest request = handlerContext.getRequest();
+
+        request.setAttribute("world", System.getProperty("user.name"));
         request.setAttribute("contextPath", request.getContextPath());
-        request.setAttribute("context", request.getContextPath().substring(1));
-        return new Destination("/WEB-INF/jsp/index.jsp");
+
+        return Destinations.forward("/WEB-INF/jsp/index.jsp");
     }
     
 }
--- agave-archetype/src/main/resources/archetype-resources/src/main/webapp/WEB-INF/jsp/index.jsp
+++ agave-archetype/src/main/resources/archetype-resources/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -4,13 +4,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
-    <title>Hello ${requestScope.context}</title>
+    <title>Hello, ${requestScope.world}!</title>
     <style type="text/css">
       @import "${contextPath}/css/structure.css";
       @import "${contextPath}/css/theme.css";
     </style>
   </head>
   <body>
-    <h1>Hello ${requestScope.context}</h1>
+    <h1>Hello, ${requestScope.world}!</h1>
   </body>
 </html>
