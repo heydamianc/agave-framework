773ad9ded74a88ab73a8c747c85c9ff9d490a6cb
Damian Carrillo <damian.carrillo@cdev.co>
2009-03-15 01:16:05 +0000

    Reformatting javadoc

--- agave/src/main/java/agave/AgaveFilter.java
+++ agave/src/main/java/agave/AgaveFilter.java
@@ -69,8 +69,8 @@ import agave.internal.URIParameterFormPopulator;
 
 
 /**
- * Scans the classes directory of a deployed context for any configured handlers,
- * builds an internal representation of all handler methods , and then 
+ * Scans the {@code classes} directory of a deployed context for any configured 
+ * handlers, builds an internal representation of all handler methods , and then 
  * forwards HTTP requests to the handlers if they match the requested URI.
  * 
  * @author <a href="mailto:damiancarrillo@gmail.com">Damian Carrillo</a>
